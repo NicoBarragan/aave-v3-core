aabb8d5b4e7a9de32596ca26af1b11885bd6f71d
NicoBarragan <nbarragan01@gmail.com>
2022-06-15 11:14:51 -0300

    feat(MockPool.sol): added 'borrow', 'supply', 'withdraw', 'repayWithATokens' mocked functions

--- contracts/mocks/helpers/MockPool.sol
+++ contracts/mocks/helpers/MockPool.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.10;
 
 import {IPoolAddressesProvider} from '../../interfaces/IPoolAddressesProvider.sol';
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract MockPool {
   // Reserved storage space to avoid layout collisions.
@@ -64,6 +65,27 @@ contract MockPool {
       uint256 healthFactor
     )
   {}
+
+  function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external {
+    require(address(msg.sender).balanceOf(asset) >= amount, "Not enough amount");
+    IERC20(asset).transferFrom(onBehalfOf, address(this), amount);
+  }
+
+  function borrow(address asset, uint256 amount, uint256 interestRateMode, uint16 referralCode, address onBehalfOf) external {
+    require(address(this).balanceOf(asset) >= amount, "Fund this mock contract with the amount");
+    IERC20(asset).transfer(onBehalfOf, amount);
+  } 
+
+  function withdraw(address asset, uint256 amount, address to) external {
+    require(address(this).balanceOf(asset) >= amount, "Fund this mock contract with the amount");
+    IERC20(asset).transfer(to, amount);
+  }
+
+  function repayWithATokens(address asset, uint256 amount, uint256 interestRateMode) external {
+    require(address(msg.sender).balanceOf(asset) >= amount, "Not enough amount");
+    IERC20(asset).transferFrom(address(msg.sender), address(this), amount);
+  }
+
 }
 
 import {Pool} from '../../protocol/pool/Pool.sol';
