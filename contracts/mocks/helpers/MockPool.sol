536712dfaeb00c6bf896d5dfd74f155349ac2703
NicoBarragan <nbarragan01@gmail.com>
2022-06-15 11:28:28 -0300

    hotfix(MockPool.sol): fixed balanceOf typo

--- contracts/mocks/helpers/MockPool.sol
+++ contracts/mocks/helpers/MockPool.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.10;
 
 import {IPoolAddressesProvider} from '../../interfaces/IPoolAddressesProvider.sol';
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 contract MockPool {
   // Reserved storage space to avoid layout collisions.
@@ -39,21 +39,22 @@ contract MockPool {
   }
 
   function setUserAccountData(
-  uint256 _totalCollateralBase,
-  uint256 _totalDebtBase,
-  uint256 _availableBorrowsBase,
-  uint256 _currentLiquidationThreshold,
-  uint256 _ltv,
-  uint256 _healthFactor) external {
-    totalCollateralBase = _totalCollateralBase,
-    totalDebtBase = _totalDebtBase,
-    availableBorrowsBase = _availableBorrowsBase,
-    currentLiquidationThreshold = _currentLiquidationThreshold,
-    ltv = _ltv,
-    healthFactor = _healthFactor
-    }
+    uint256 _totalCollateralBase,
+    uint256 _totalDebtBase,
+    uint256 _availableBorrowsBase,
+    uint256 _currentLiquidationThreshold,
+    uint256 _ltv,
+    uint256 _healthFactor
+  ) external {
+    totalCollateralBase = _totalCollateralBase;
+    totalDebtBase = _totalDebtBase;
+    availableBorrowsBase = _availableBorrowsBase;
+    currentLiquidationThreshold = _currentLiquidationThreshold;
+    ltv = _ltv;
+    healthFactor = _healthFactor;
+  }
 
-    function getUserAccountData(address user)
+  function getUserAccountData(address user)
     external
     view
     returns (
@@ -66,26 +67,50 @@ contract MockPool {
     )
   {}
 
-  function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external {
-    require(address(msg.sender).balanceOf(asset) >= amount, "Not enough amount");
+  function supply(
+    address asset,
+    uint256 amount,
+    address onBehalfOf,
+    uint16 referralCode
+  ) external {
+    require(IERC20(asset).balanceOf(msg.sender) >= amount, 'Not enough amount');
     IERC20(asset).transferFrom(onBehalfOf, address(this), amount);
   }
 
-  function borrow(address asset, uint256 amount, uint256 interestRateMode, uint16 referralCode, address onBehalfOf) external {
-    require(address(this).balanceOf(asset) >= amount, "Fund this mock contract with the amount");
+  function borrow(
+    address asset,
+    uint256 amount,
+    uint256 interestRateMode,
+    uint16 referralCode,
+    address onBehalfOf
+  ) external {
+    require(
+      IERC20(asset).balanceOf(msg.sender) >= amount,
+      'Fund this mock contract with the amount'
+    );
     IERC20(asset).transfer(onBehalfOf, amount);
-  } 
+  }
 
-  function withdraw(address asset, uint256 amount, address to) external {
-    require(address(this).balanceOf(asset) >= amount, "Fund this mock contract with the amount");
+  function withdraw(
+    address asset,
+    uint256 amount,
+    address to
+  ) external {
+    require(
+      IERC20(asset).balanceOf(msg.sender) >= amount,
+      'Fund this mock contract with the amount'
+    );
     IERC20(asset).transfer(to, amount);
   }
 
-  function repayWithATokens(address asset, uint256 amount, uint256 interestRateMode) external {
-    require(address(msg.sender).balanceOf(asset) >= amount, "Not enough amount");
+  function repayWithATokens(
+    address asset,
+    uint256 amount,
+    uint256 interestRateMode
+  ) external {
+    require(IERC20(asset).balanceOf(msg.sender) >= amount, 'Not enough amount');
     IERC20(asset).transferFrom(address(msg.sender), address(this), amount);
   }
-
 }
 
 import {Pool} from '../../protocol/pool/Pool.sol';
